5f98e0b7153936e35033f8f434e17e336475c9c2
Michael Wu <michael.yixuan.wu@gmail.com>
2015-12-22 14:28:46 -0500

    explicitly adding git completion

--- .standard.bash
+++ .standard.bash
@@ -13,6 +13,7 @@ fi
 if [ "$platform" == "OSX" ]; then
     if [ -f $(brew --prefix)/etc/bash_completion ]; then
         . $(brew --prefix)/etc/bash_completion
+        . $(brew --prefix)/etc/bash_completion.d/git-completion.bash
         export GIT_PS1_SHOWDIRTYSTATE=1
         export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\w \[\033[01;33m\]$(__git_ps1) \[\033[01;34m\]> \[\033[00m\]'
     else
