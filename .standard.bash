ff179ba8d6b8cb49d1b9dece000439015698b297
Michael Wu <michael.yixuan.wu@gmail.com>
2015-12-22 14:16:53 -0500

    edited bash completion sourcing

--- .standard.bash
+++ .standard.bash
@@ -11,8 +11,8 @@ fi
 
 # Bash prompt
 if [ "$platform" == "OSX" ]; then
-    if [ -f /usr/local/etc/bash_completion ]; then
-        . /usr/local/etc/bash_completion
+    if [ -f $(brew --prefix)/etc/bash_completion ]; then
+        . $(brew --prefix)/etc/bash_completion
         export GIT_PS1_SHOWDIRTYSTATE=1
         export PS1='\[\033[01;32m\]\u@\h \[\033[01;34m\]\w \[\033[01;33m\]$(__git_ps1) \[\033[01;34m\]> \[\033[00m\]'
     else
