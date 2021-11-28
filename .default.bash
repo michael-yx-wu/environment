e302cb3d9068864679bd352f88f590ff801e742a
Michael Wu <michael-yx-wu@users.noreply.github.com>
2021-11-28 13:50:29 -0500

    Make compatible with linux

--- .default.bash
+++ .default.bash
@@ -1,16 +1,28 @@
 #!/usr/bin/env bash
 
 # Determine OS
-platform='unknown'
+IS_MACOS=false
+IS_LINUX=false
 if [ "$(uname)" == 'Darwin' ]; then
-    platform="OSX"
+    IS_MACOS=true
 elif [ "$(uname)" == 'Linux' ]; then
-    platform="Linux"
+    IS_LINUX=true
 fi
 
+# Language version management init
+export PATH="$HOME/.jenv/bin:$PATH"
+eval "$(jenv init -)"
 eval "$(rbenv init -)"
+export PYENV_ROOT="$HOME/.pyenv"
+export PATH="$PYENV_ROOT/bin:$PATH"
+alias pyinit='eval "$(pyenv init --path)"'
+if $IS_LINUX; then
+    export PATH="$HOME/.nodenv/bin:$PATH"
+    # Not auto-initialized because homebrew historically installed things in weird locations if
+    # pyenv was initialized -- worth checking to see if this is still necessary
+    pyinit
+fi
 eval "$(nodenv init -)"
-alias pyinit='eval "$(pyenv init -)"'
 
 # Add /usr/local/sbin to PATH for Homebrew
 export PATH="/usr/local/sbin:$PATH"
@@ -18,10 +30,6 @@ export PATH="/usr/local/sbin:$PATH"
 # Node / NPM
 NODE_PATH="$(npm prefix -g)/lib/node_modules"
 export NODE_PATH
-
-# Java
-export PATH="$HOME/.jenv/bin:$PATH"
-eval "$(jenv init -)"
 
 # Append current directory to path
 export PATH=$PATH:.
@@ -33,34 +41,48 @@ export HISTFILESIZE="$HISTSIZE"
 shopt -s histappend
 PROMPT_COMMAND="history -a; history -n; $PROMPT_COMMAND"
 
-# PS1 and GIT_PS1 customization
-if [ "$platform" == 'OSX' ]; then
-    export PROMPT_DIRTRIM=3
+# Auto completion and prompt customization
+export GIT_PS1_SHOWDIRTYSTATE=1
+# shellcheck disable=SC2016
+PROMPT_WITH_GIT='\[\033[01;32m\]\u \[\033[01;34m\]\w\[\033[01;33m\]$(__git_ps1) \[\033[01;34m\]> \[\033[00m\]'
+PROMPT_WITHOUT_GIT='\[\033[01;32m\]\u \[\033[01;34m\]\w \[\033[01;34m\]> \[\033[00m\]'
+WARN_NO_GIT_PROMPT='\e[1;33mbash_completion not found: prompt will not include __git_ps1\e[0m'
+if $IS_MACOS; then
     if [ -f "$(brew --prefix)/etc/bash_completion" ]; then
-        # shellcheck source=/dev/null
-        source $(brew --prefix)/etc/bash_completion
-        # shellcheck source=/dev/null
-        source $(brew --prefix)/etc/bash_completion.d/git-completion.bash
-        export GIT_PS1_SHOWDIRTYSTATE=1
-        export PS1='\[\033[01;32m\]\u \[\033[01;34m\]\w\[\033[01;33m\]$(__git_ps1) \[\033[01;34m\]> \[\033[00m\]'
+        # shellcheck disable=SC1091
+        source "$(brew --prefix)/etc/bash_completion"
+        # shellcheck disable=SC1091
+        source "$(brew --prefix)/etc/bash_completion.d/git-completion.bash"
+        export PS1=$PROMPT_WITH_GIT
     else
-        echo 'Warn: bash_completion not found'
-        export PS1='\[\033[01;32m\]\u \[\033[01;34m\]\w \[\033[01;34m\]> \[\033[00m\]'
+        echo -e "$WARN_NO_GIT_PROMPT"
+        export PS1=$PROMPT_WITHOUT_GIT
+    fi
+elif $IS_LINUX; then
+    if [ -f /etc/bash_completion ]; then
+        source /etc/bash_completion
+        for BASH_COMPLETION_FILE in /etc/bash_completion.d/* ; do
+            # shellcheck disable=SC1090
+            source "$BASH_COMPLETION_FILE"
+        done
+        export PS1=$PROMPT_WITH_GIT
+    else
+        echo -e "$WARN_NO_GIT_PROMPT"
+        export PS1=$PROMPT_WITHOUT_GIT
     fi
 else
-    echo 'Non-macOS platform: not customizing bash prompt'
+    echo -e '\e[1;33mUnknown platform: not customizing prompt\e[0m'
 fi
 
 # Highlighting
 export GREP_OPTIONS='--color=auto'
-if [ "$platform" == 'OSX' ]; then
-    export LSCOLORS="Exfxcxdxbxegedabagacad"
-    alias ls="ls -G"
-elif [ "$platform" == 'Linux' ]; then
-    export LS_COLORS="Exfxcxdxbxegedabagacad"
+if $IS_MACOS; then
+    export CLICOLOR=1
+    export LSCOLORS='Exfxcxdxbxegedabagacad'
+elif $IS_LINUX; then
     alias ls="ls --color=auto"
 else
-    echo 'Unknown platform: not setting alias for ls'
+    echo -e '\e[1;33mUnknown platform: not setting ls colors\e[0m'
 fi
 
 # Navigation
